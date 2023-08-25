6ccd80ac439d7f22085bcd607c59ce6cd3186ece
Matheus Kenzo <100180936+Dobat00@users.noreply.github.com>
2023-08-24 21:03:39 -0300

    criacao da funcao de novo projeto

--- src/screen.js
+++ src/screen.js
@@ -14,14 +14,25 @@ const createScreen = () => {
     // inputField.classList.add('inputField')
     // divInput.appendChild(inputField)
 
-    let inputButton = document.createElement('button');
-    inputButton.classList.add('inputButton');
-    inputButton.textContent = 'Novo projeto'
-    inputButton.addEventListener('click', (e)=>{
+    let projetoButton = document.createElement('button');
+    projetoButton.classList.add('inputButton');
+    projetoButton.textContent = 'Novo projeto'
+
+    projetoButton.addEventListener('click', (e)=>{
+        projetoButton.disabled = true;
+
+        let divProjeto = gerarProjeto();
+        divContent.appendChild(divProjeto)
+    })
+
+    divInput.appendChild(projetoButton)
+
+    const gerarProjeto = () =>{
         let divProjeto = document.createElement('div')
         divProjeto.classList.add('divProjeto')
 
         let tituloLabel = document.createElement('label')
+        tituloLabel.textContent = 'Titulo';
         tituloLabel.setAttribute('for', 'tituloInput')
         divProjeto.appendChild(tituloLabel)
 
@@ -29,10 +40,35 @@ const createScreen = () => {
         tituloInput.setAttribute('id', 'tituloInput')
         divProjeto.appendChild(tituloInput)
 
-        divContent.appendChild(divProjeto)
-    })
+        let descricaoLabel = document.createElement('label')
+        descricaoLabel.textContent = 'Descricao';
+        descricaoLabel.setAttribute('for', 'descricaoInput')
+        divProjeto.appendChild(descricaoLabel)
 
-    divInput.appendChild(inputButton)
+        let descricaoInput = document.createElement('input')
+        descricaoInput.setAttribute('id', 'descricaoInput')
+        divProjeto.appendChild(descricaoInput)
+
+        let dataLabel = document.createElement('label')
+        dataLabel.textContent = 'Data limite';
+        dataLabel.setAttribute('for', 'dataInput')
+        divProjeto.appendChild(dataLabel)
+
+        let dataInput = document.createElement('input')
+        descricaoInput.setAttribute('id', 'dataInput')
+        divProjeto.appendChild(dataInput)
+
+        let importanciaLabel = document.createElement('label')
+        importanciaLabel.textContent = 'Importancia';
+        importanciaLabel.setAttribute('for', 'importanciaInput')
+        divProjeto.appendChild(importanciaLabel)
+
+        let importanciaInput = document.createElement('input')
+        importanciaInput.setAttribute('id', 'importanciaImput')
+        divProjeto.appendChild(importanciaInput)
+
+        return divProjeto;
+    }
 
 
 }
